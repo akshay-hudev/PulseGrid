6ac77f17a8211c3bd88406298e86932479952abf
akshay-hudev <akshayhudevmanjunath@gmail.com>
2026-08-27 13:13:28 +0530

    Fix Vercel build by removing standalone output

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  //output: "standalone",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
